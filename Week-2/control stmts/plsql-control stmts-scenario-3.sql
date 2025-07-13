a7f4417a54b27ad7e545e8acaf538cd39278e455
pavithra duddu <duddupavithra3@gmail.com>
2025-07-13 20:52:45 +0530

    week 4

--- Week-2/control stmts/plsql-control stmts-scenario-3.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  CONCAT('Reminder: Dear ', c.Name, 
-         ', your loan (Loan ID: ', l.LoanID, 
-         ') is due on ', DATE_FORMAT(l.EndDate, '%d-%b-%Y')) AS ReminderMessage
-FROM Loans l
-JOIN Customers c ON l.CustomerID = c.CustomerID
-WHERE l.EndDate BETWEEN CURDATE() AND CURDATE() + INTERVAL 30 DAY;
